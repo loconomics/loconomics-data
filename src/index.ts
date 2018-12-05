c0fefe9525ea83ed1ce7e98098e2611e68e26e23
IagoSRL <iagosrl@gmail.com>
2018-12-05 10:21:39 +0100

    Ignoring local environment vars in file, plus notice of success connection

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 
 createConnection().then(async connection => {
+    console.log('Connected');
 }).catch(error => console.log(error));
