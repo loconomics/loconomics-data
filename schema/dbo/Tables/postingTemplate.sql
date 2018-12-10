0e783159c36c21c81fcd9ff43a288442dbe9ad19
Nolan Darilek <nolan@thewordnerd.info>
2018-12-10 14:27:18 -0600

    Drop `LanguageID` and `CountryID`, replacing with `language`.

--- schema/dbo/Tables/postingTemplate.sql
+++ schema/dbo/Tables/postingTemplate.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TABLE [dbo].[postingTemplate] (
     [postingTemplateID] INT                NOT NULL,
     [name]              NVARCHAR (200)     NOT NULL,
-    [languageID]        INT                NOT NULL,
-    [countryID]         INT                NOT NULL,
+    [language]      NVARCHAR (42) NOT NULL,
     [createdDate]       DATETIMEOFFSET (0) NOT NULL,
     [updatedDate]       DATETIMEOFFSET (0) NOT NULL,
     [modifiedBy]        NCHAR (5)          NOT NULL,
