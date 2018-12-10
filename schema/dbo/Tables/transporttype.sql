468d5ce5405023d69f34536a2c70759c90dc97d2
Nolan Darilek <nolan@thewordnerd.info>
2018-12-10 14:14:21 -0600

    Drop `LanguageID` and `CountryID`, replacing with `language`.

--- schema/dbo/Tables/transporttype.sql
+++ schema/dbo/Tables/transporttype.sql
@@ -1,9 +1,8 @@
 ﻿CREATE TABLE [dbo].[transporttype] (
     [TransportTypeID]          INT           NOT NULL,
-    [LanguageID]               INT           NOT NULL,
-    [CountryID]                INT           NOT NULL,
     [TransportTypeName]        VARCHAR (50)  NOT NULL,
     [TransportTypeDescription] VARCHAR (300) NULL,
+    [language]      NVARCHAR (42) NOT NULL,
     [CreatedDate]              DATETIME      NOT NULL,
     [UpdatedDate]              DATETIME      NOT NULL,
     [ModifiedBy]               VARCHAR (25)  NOT NULL,
