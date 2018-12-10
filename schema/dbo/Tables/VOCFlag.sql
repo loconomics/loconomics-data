61472d06b8a4351d37f81184e865ff62e6b8cd9e
Nolan Darilek <nolan@thewordnerd.info>
2018-12-10 14:04:52 -0600

    Drop `LanguageID` and `CountryID`, replacing with `language`.

--- schema/dbo/Tables/VOCFlag.sql
+++ schema/dbo/Tables/VOCFlag.sql
@@ -1,9 +1,8 @@
 ﻿CREATE TABLE [dbo].[VOCFlag] (
     [VOCFlagID]           INT           NOT NULL,
-    [LanguageID]          INT           NOT NULL,
-    [CountryID]           INT           NOT NULL,
     [VOCFlagName]         VARCHAR (50)  NOT NULL,
     [VOCFlagNDescription] VARCHAR (500) NULL,
+    [language]      NVARCHAR (42) NOT NULL,
     [CreateDate]          DATETIME      NOT NULL,
     [UpdatedDate]         DATETIME      NOT NULL,
     [ModifiedBy]          VARCHAR (3)   NOT NULL,
