9a54c0dd976a10160007840b86749e82bd6232f4
Nolan Darilek <nolan@thewordnerd.info>
2018-12-10 14:15:57 -0600

    Drop `LanguageID` and `CountryID`, replacing with `language`.

--- schema/dbo/Tables/Specialization.sql
+++ schema/dbo/Tables/Specialization.sql
@@ -1,10 +1,9 @@
 ﻿CREATE TABLE [dbo].[Specialization] (
     [SpecializationID] INT                NOT NULL,
-    [LanguageID]       INT                NOT NULL,
-    [CountryID]        INT                NOT NULL,
     [SolutionID]       INT                NOT NULL,
     [Name]             NVARCHAR (100)     NOT NULL,
     [DisplayRank]      INT                NULL,
+    [language]      NVARCHAR (42) NOT NULL,
     [CreatedDate]      DATETIMEOFFSET (7) NOT NULL,
     [UpdatedDate]      DATETIMEOFFSET (7) NOT NULL,
     [CreatedBy]        NVARCHAR (12)      CONSTRAINT [DF__Specializ__Creat__23D42350] DEFAULT ('staff') NOT NULL,
