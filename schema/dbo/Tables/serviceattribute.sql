7f7e9e56d4424fba2e06fee20be1e69887b8c76d
Nolan Darilek <nolan@thewordnerd.info>
2018-12-10 14:20:54 -0600

    Drop `LanguageID` and `CountryID`, replacing with `language`.

--- schema/dbo/Tables/serviceattribute.sql
+++ schema/dbo/Tables/serviceattribute.sql
@@ -1,10 +1,9 @@
 ﻿CREATE TABLE [dbo].[serviceattribute] (
     [ServiceAttributeID]          INT            NOT NULL,
-    [LanguageID]                  INT            NOT NULL,
-    [CountryID]                   INT            NOT NULL,
     [SourceID]                    INT            DEFAULT (NULL) NULL,
     [Name]                        VARCHAR (100)  NULL,
     [ServiceAttributeDescription] VARCHAR (2000) NULL,
+    [language]      NVARCHAR (42) NOT NULL,
     [CreateDate]                  DATETIME       NULL,
     [UpdatedDate]                 DATETIME       NULL,
     [ModifiedBy]                  VARCHAR (45)   NULL,
