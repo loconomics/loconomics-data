e448b9876596fe62784963a0f758a87a6bd6f1ff
Nolan Darilek <nolan@thewordnerd.info>
2018-12-10 14:08:03 -0600

    Drop `LanguageID` and `CountryID`, replacing with `language`.

--- schema/dbo/Tables/verificationstatus.sql
+++ schema/dbo/Tables/verificationstatus.sql
@@ -1,9 +1,8 @@
 ﻿CREATE TABLE [dbo].[verificationstatus] (
     [VerificationStatusID]                 INT           NOT NULL,
-    [LanguageID]                           INT           NOT NULL,
-    [CountryID]                            INT           NOT NULL,
     [VerificationStatusName]               VARCHAR (50)  NOT NULL,
     [VerificationStatusDisplayDescription] VARCHAR (300) NULL,
+    [language]      NVARCHAR (42) NOT NULL,
     [CreatedDate]                          DATETIME      NOT NULL,
     [UpdatedDate]                          DATETIME      NOT NULL,
     [ModifiedBy]                           VARCHAR (25)  NOT NULL,
