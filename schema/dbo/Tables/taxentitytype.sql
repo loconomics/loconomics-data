3ffda777213dd18a467d2d62718fb5efcf8e7dbd
Nolan Darilek <nolan@thewordnerd.info>
2018-12-10 14:15:17 -0600

    Drop `LanguageID` and `CountryID`, replacing with `language`.

--- schema/dbo/Tables/taxentitytype.sql
+++ schema/dbo/Tables/taxentitytype.sql
@@ -1,9 +1,8 @@
 ﻿CREATE TABLE [dbo].[taxentitytype] (
     [TaxEntityTypeID]          INT           NOT NULL,
-    [LanguageID]               INT           NOT NULL,
-    [CountryID]                INT           NOT NULL,
     [TaxEntityTypeName]        VARCHAR (75)  NULL,
     [TaxEntityTypeDescription] VARCHAR (300) NULL,
+    [language]      NVARCHAR (42) NOT NULL,
     [CreatedDate]              DATETIME      NOT NULL,
     [UpdatedDate]              DATETIME      NOT NULL,
     [ModifiedBy]               VARCHAR (25)  NOT NULL,
