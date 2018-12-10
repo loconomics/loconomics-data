b0ac35a5eb59ac5d01491d1d0d31970ae8dcda72
Nolan Darilek <nolan@thewordnerd.info>
2018-12-10 12:43:20 -0600

    Exit on error, and support patching entities for cases where schema modifications aren't enough.

--- regenerate-all.sh
+++ regenerate-all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 TYPEORM_DATABASE=${TYPEORM_DATABASE:=loconomics}
 TYPEORM_USERNAME=${TYPEORM_USERNAME:=sa}
 TYPEORM_PASSWORD=${TYPEORM_PASSWORD:=Password!!11}
@@ -14,4 +16,6 @@ echo Create DB $TYPEORM_DATABASE
 sqlcmd -U "$TYPEORM_USERNAME" -P "$TYPEORM_PASSWORD" -S "$TYPEORM_HOST" -i ./schema.sql
 echo Regenerate entity models for $TYPEORM_DATABASE
 yarn typeorm-model-generator -d $TYPEORM_DATABASE -u $TYPEORM_USERNAME -x $TYPEORM_PASSWORD -e $TYPEORM_ENGINE -h $TYPEORM_HOST --ce=pascal --cp=camel --lazy=true --noConfig -o ./src/entities
+echo Patching entities
+cp src/entities.patch/* src/entities
 echo DONE
