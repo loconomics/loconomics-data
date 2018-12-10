1298da39328c8b1141bca0ad069293151518ece2
Nolan Darilek <nolan@thewordnerd.info>
2018-12-10 14:23:06 -0600

    Drop `LanguageID` and `CountryID`, replacing with `language`.

--- schema/dbo/Tables/SearchCategory.sql
+++ schema/dbo/Tables/SearchCategory.sql
@@ -1,21 +1,16 @@
 ﻿CREATE TABLE [dbo].[SearchCategory] (
     [SearchCategoryID] INT                NOT NULL,
-    [LanguageID]       INT                NOT NULL,
-    [CountryID]        INT                NOT NULL,
     [Name]             NVARCHAR (20)      NOT NULL,
     [ShortDescription] NVARCHAR (100)     NULL,
     [LongDescription]  NVARCHAR (300)     NULL,
     [SmallImage]       NVARCHAR (255)     NULL,
     [BannerImage]      NVARCHAR (255)     NULL,
     [DisplayRank]      INT                NULL,
+    [language]      NVARCHAR (42) NOT NULL,
     [CreatedDate]      DATETIMEOFFSET (7) NOT NULL,
     [UpdatedDate]      DATETIMEOFFSET (7) NOT NULL,
     [ModifiedBy]       NVARCHAR (4)       DEFAULT ('sys') NOT NULL,
     [Active]           BIT                DEFAULT ((1)) NOT NULL,
-    PRIMARY KEY CLUSTERED ([SearchCategoryID] ASC, [LanguageID] ASC, [CountryID] ASC)
+    PRIMARY KEY CLUSTERED ([SearchCategoryID] ASC)
 );
 
-GO
-
-ALTER TABLE [dbo].[SearchCategory] ADD
-    CONSTRAINT [FK_SearchCategory_language] FOREIGN KEY ([LanguageID], [CountryID]) REFERENCES [dbo].[language] ([LanguageID], [CountryID])
