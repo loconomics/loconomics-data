d1f4aa6d30d55a0e928b92a6e6e53256f2e32243
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 12:08:15 -0600

    Convert `UserPaymentPlan`.

--- src/entities/UserPaymentPlan.ts
+++ src/entities/UserPaymentPlan.ts
@@ -1,4 +1,17 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("UserPaymentPlan",{schema:"dbo"})
 export class UserPaymentPlan {
