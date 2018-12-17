c2ac9214f3f274601d280fef8440c3d3d3289b42
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 10:01:20 -0600

    Convert `usersignup`.

--- src/entities/UserSignup.ts
+++ src/entities/UserSignup.ts
@@ -1,8 +1,21 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("usersignup",{schema:"dbo"})
 @Index("UQ__usersign__A9D10534C226C65B",["email"],{unique:true})
-export class Usersignup {
+export class UserSignup {
 
     @PrimaryGeneratedColumn({
         name:"UserId",
