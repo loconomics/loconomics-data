b8469366917b64adedc51db830eb16efa94ab42d
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 12:00:22 -0600

    Convert UserReviews.

--- src/entities/UserReview.ts
+++ src/entities/UserReview.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("UserReviews",{schema:"dbo"})
-export class UserReviews {
+export class UserReview {
 
     @Column("int",{
         nullable:false,
