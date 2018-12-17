9746b5da9d453d2f37f4de7b443b51aa63442b78
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 10:59:40 -0600

    Convert `providerpackagedetail`.

--- src/entities/ProviderPackageDetail.ts
+++ src/entities/ProviderPackageDetail.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("providerpackagedetail",{schema:"dbo"})
-export class Providerpackagedetail {
+export class ProviderPackageDetail {
 
     @Column("int",{
         nullable:false,
