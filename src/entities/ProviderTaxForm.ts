50f1d3a75b7ad86757fb19d50bb430f4bf584694
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 10:53:43 -0600

    Convert `providertaxform`.

--- src/entities/ProviderTaxForm.ts
+++ src/entities/ProviderTaxForm.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("providertaxform",{schema:"dbo"})
-export class Providertaxform {
+export class ProviderTaxForm {
 
     @Column("int",{
         nullable:false,
