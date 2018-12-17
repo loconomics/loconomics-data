218048ec7a95c876dd009f8ac0ea34358672be06
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 12:02:01 -0600

    Convert `UserReviewScores`.

--- src/entities/UserReviewScore.ts
+++ src/entities/UserReviewScore.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("UserReviewScores",{schema:"dbo"})
-export class UserReviewScores {
+export class UserReviewScore {
 
     @Column("int",{
         nullable:false,
