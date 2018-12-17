d8ea102a1b613b43c9d7a86af1ba0317c47c29bd
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 11:52:35 -0600

    Convert `VOCFlag`.

--- src/entities/VOCFlag.ts
+++ src/entities/VOCFlag.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("VOCFlag",{schema:"dbo"})
-export class VocFlag {
+export class VOCFlag {
 
     @Column("int",{
         nullable:false,
