082b282656dd5630b381c4398ae50d9e027202f1
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 12:29:03 -0600

    Convert `UserBadge`.

--- src/entities/UserBadge.ts
+++ src/entities/UserBadge.ts
@@ -1,4 +1,17 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 import {Solution} from "./Solution";
 import {User} from "./User"
