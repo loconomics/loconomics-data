e5850230f93327d726d9eded13e4095ce3fd520f
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 10:56:17 -0600

    Convert `providerpaymentpreferencetype`.

--- src/entities/ProviderPaymentPreferenceType.ts
+++ src/entities/ProviderPaymentPreferenceType.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("providerpaymentpreferencetype",{schema:"dbo"})
-export class Providerpaymentpreferencetype {
+export class ProviderPaymentPreferenceType {
 
     @Column("int",{
         nullable:false,
