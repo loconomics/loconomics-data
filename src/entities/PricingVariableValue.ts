ba0eeb7524d0a3d55e8ee9ba47108fc8b31b5bf8
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 12:57:52 -0600

    Migrate `PricingVariableValue`.

--- src/entities/PricingVariableValue.ts
+++ src/entities/PricingVariableValue.ts
@@ -1,4 +1,17 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("PricingVariableValue",{schema:"dbo"})
 export class PricingVariableValue {
