df956df6b3d7441ecf587c5a0e8c9a6bee6c7c70
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 09:34:45 -0600

    Convert `XJobTitleReviewRule`.

--- src/entities/XJobTitleReviewRule.ts
+++ src/entities/XJobTitleReviewRule.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("xJobTitleReviewRules",{schema:"dbo"})
-export class XJobTitleReviewRules {
+export class XJobTitleReviewRule {
 
     @Column("int",{
         nullable:false,
