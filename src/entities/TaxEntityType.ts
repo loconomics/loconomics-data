2ead1df261f0487b2568eab20fca583eaae3f528
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 10:29:39 -0600

    Convert `taxentitytype`.

--- src/entities/TaxEntityType.ts
+++ src/entities/TaxEntityType.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("taxentitytype",{schema:"dbo"})
-export class Taxentitytype {
+export class TaxEntityType {
 
     @Column("int",{
         nullable:false,
