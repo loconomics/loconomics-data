58fd6478af9350bc06d4db80ac183ec3dc0a0c64
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 12:56:51 -0600

    Migrate `ProviderPaymentAccount`.

--- src/entities/ProviderPaymentAccount.ts
+++ src/entities/ProviderPaymentAccount.ts
@@ -1,4 +1,17 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("ProviderPaymentAccount",{schema:"dbo"})
 export class ProviderPaymentAccount {
