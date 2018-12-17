1d2b2960f59c6d0850cca48eab201df4709b1996
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 09:42:09 -0600

    Convert `webpages_OAuthMembership`.

--- src/entities/WebpageOAuthMembership.ts
+++ src/entities/WebpageOAuthMembership.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("webpages_OAuthMembership",{schema:"dbo"})
-export class WebpagesOAuthMembership {
+export class WebpageOAuthMembership {
 
     @Column("nvarchar",{
         nullable:false,
