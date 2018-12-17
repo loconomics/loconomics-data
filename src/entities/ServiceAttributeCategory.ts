d10b4682fa6639bee095a1d21112cb40acad4ea8
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 10:40:50 -0600

    Convert `serviceattributecategory`.

--- src/entities/ServiceAttributeCategory.ts
+++ src/entities/ServiceAttributeCategory.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("serviceattributecategory",{schema:"dbo"})
-export class Serviceattributecategory {
+export class ServiceAttributeCategory {
 
     @Column("int",{
         nullable:false,
