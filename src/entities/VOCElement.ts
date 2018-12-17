5f761ce5f1055d2ac9341b38ab4ab4b1ddf0b62b
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 11:56:02 -0600

    Convert `VOCElement`.

--- src/entities/VOCElement.ts
+++ src/entities/VOCElement.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("VOCElement",{schema:"dbo"})
-export class VocElement {
+export class VOCElement {
 
     @Column("int",{
         nullable:false,
