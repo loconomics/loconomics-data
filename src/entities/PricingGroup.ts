bfacced71bb6c2259a33b78793ec0b4501688878
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 12:59:57 -0600

    Migrate `PricingGroups`.

--- src/entities/PricingGroup.ts
+++ src/entities/PricingGroup.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("PricingGroups",{schema:"dbo"})
-export class PricingGroups {
+export class PricingGroup {
 
     @Column("int",{
         nullable:false,
