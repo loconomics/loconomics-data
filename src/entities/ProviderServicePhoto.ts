2431ce950de99eb1843a749c0990306058bbdb67
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 10:54:55 -0600

    Convert `providerservicephoto`.

--- src/entities/ProviderServicePhoto.ts
+++ src/entities/ProviderServicePhoto.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("providerservicephoto",{schema:"dbo"})
-export class Providerservicephoto {
+export class ProviderServicePhoto {
 
     @PrimaryGeneratedColumn({
         name:"ProviderServicePhotoID",
