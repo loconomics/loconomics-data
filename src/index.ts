4d06bd48af70bd5c7941408d534ffd76a8f7f802
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 11:45:22 -0600

    Convert `address`.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {createConnection} from "typeorm"
 
 export default createConnection
 
+export {Address} from "./entities/Address"
 export {AddressType} from "./entities/AddressType"
 export {Alert} from "./entities/Alert"
 export {AlertType} from "./entities/AlertType"
