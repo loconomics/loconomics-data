75bc04ce11dc006d1f6efcbed0eb3c51e191aaa0
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 11:00:41 -0600

    Convert `providerpackage`.

--- src/entities/ProviderPackage.ts
+++ src/entities/ProviderPackage.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("providerpackage",{schema:"dbo"})
-export class Providerpackage {
+export class ProviderPackage {
 
     @PrimaryGeneratedColumn({
         name:"ProviderPackageID",
