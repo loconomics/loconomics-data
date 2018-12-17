5ad85c9f4177b64f48b41410df52bd5662fee644
Nolan Darilek <nolan@thewordnerd.info>
2018-12-17 11:11:37 -0600

    Convert `PositionPricingType`.

--- src/entities/PositionPricingType.ts
+++ src/entities/PositionPricingType.ts
@@ -1,7 +1,20 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("positionpricingtype",{schema:"dbo"})
-export class Positionpricingtype {
+export class PositionPricingType {
 
     @Column("int",{
         nullable:false,
