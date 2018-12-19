8246e696345d555154ef5989f48cd23bfe5b0eaa
Nolan Darilek <nolan@thewordnerd.info>
2018-12-19 11:53:50 -0600

    Convert `ExperienceLevel`.

--- src/entities/ExperienceLevel.ts
+++ src/entities/ExperienceLevel.ts
@@ -1,4 +1,17 @@
-import {Column,Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId} from "typeorm";
+import {
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    RelationId,
+} from "typeorm";
 
 @Entity("ExperienceLevel",{schema:"dbo"})
 export class ExperienceLevel {
